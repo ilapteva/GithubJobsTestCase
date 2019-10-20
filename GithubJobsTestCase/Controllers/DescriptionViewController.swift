fc2095ad7576e20683386bb7e0e1a6025ac0e653
Irina Lapteva <iralapteva44@gmail.com>
2019-10-21 02:44:33 +0300

    Update descriptionViewController.

--- GithubJobsTestCase/Controllers/DescriptionViewController.swift
+++ GithubJobsTestCase/Controllers/DescriptionViewController.swift
@@ -13,12 +13,27 @@ class DescriptionViewController: UIViewController {
     
     var job: Job?
     
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
-//
-//    descriptionLabel.text = job.description
-    
-   
-    
-  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        descriptionTextView.attributedText = job!.description?.htmlToAttributedString
+    }
+
+
+}
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return NSAttributedString()
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
 }
